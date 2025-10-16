3f15c0144a2e138c1d60bb342f50953aa9034a6f
Javier Riveros <walter.riveros.c@gmail.com>
2025-10-16 15:24:28 -0500

    Initial commit

---
+++ Aural/AuralApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuralApp.swift
+//  Aural
+//
+//  Created by Javier Riveros on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AuralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
